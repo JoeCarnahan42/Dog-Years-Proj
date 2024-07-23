bd9737453e749efbb2679ed49da00cc9d29d4816
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2024-07-23 15:44:05 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+let myAge = 28
+
+// Returns my name, but in all lowercase
+const myName = "JOE".toLowerCase()
+
+// First 2 years of a dogs life
+earlyYears = 2
+earlyYears = earlyYears * 10.5
+
+// Removing years already calculated from total
+let laterYears = myAge - 2
+
+// Dog years after first 2 years
+laterYears *= 4
+
+// Adds both equations to total dog years conversion
+myAgeInDogYears = earlyYears + laterYears
+
+// Logs the statement with my variables
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
